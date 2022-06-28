20e234d16d76f3514ab3c3afbd4fee6d94568a66
P3t3rtm <33187689+P3t3rtm@users.noreply.github.com>
2022-06-27 23:03:26 -0600

    anti spamming

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -5,23 +5,28 @@ const int LIGHT_PIN = 11;   // Arduino pin connected to the LIGHT
 // IRsendRaw mySender;
 
 // variables will change:
-int ledState = HIGH;     // the current state of LED
+int ledState = LOW;      // the current state of LED low == NC switch == lights off
 int lastButtonState;     // the previous state of button
 int currentButtonState;  // the current state of button
+
+long lasttime = 0;
 
 void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);  // set arduino pin to input pull-up mode
   pinMode(LIGHT_PIN, OUTPUT);         // set arduino pin to output mode
-  digitalWrite(LIGHT_PIN, ledState); //defaults to lights ON
+  //digitalWrite(LIGHT_PIN, ledState); //defaults to lights ON
 }
 
 void loop() {
-  delay(200);                                    //anti switch bumping
-  lastButtonState = currentButtonState;          // save the last state
+  //add a delay to prevent switch bumping
+
+  // save the last state
   currentButtonState = digitalRead(BUTTON_PIN);  // read new state
 
-  if (lastButtonState == HIGH && currentButtonState == LOW) {
-    // mySender.send(rawData20C,RAW_DATA_LEN,38);  //Pass the buffer,length, optionally frequency
+
+  //button state high = switch not pressed
+  if (lastButtonState == HIGH && currentButtonState == LOW && ((millis() - lasttime) > 2000)) {
+    lasttime = millis();
 
     // toggle state of LED
     ledState = !ledState;
@@ -29,5 +34,5 @@ void loop() {
     // control LED arccoding to the toggled state
     digitalWrite(LIGHT_PIN, ledState);
   }
+  lastButtonState = currentButtonState;
 }
-

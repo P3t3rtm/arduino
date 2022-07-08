94ccf40d84b5def2edc10e8e15bcf95a784491fc
P3t3rtm <33187689+P3t3rtm@users.noreply.github.com>
2022-07-08 02:36:24 -0600

    still needs to figure out EMI problem

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -5,18 +5,21 @@ const int LIGHT_PIN = 11;   // Arduino pin connected to the LIGHT
 // IRsendRaw mySender;
 
 // variables will change:
-int ledState;  // the current state of LED low == NC switch == lights off
-int ledState2;
-int ledState3;
-int lastButtonState;     // the previous state of button
-int currentButtonState;  // the current state of button
+bool ledState;  // the current state of LED low == NC switch == lights off
+bool ledState2;
+bool ledState3;
+bool lastButtonState;     // the previous state of button
+bool currentButtonState;  // the current state of button
 
 unsigned long lasttime = 0;
 
 void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);  // set arduino pin to input pull-up mode
   pinMode(LIGHT_PIN, OUTPUT);         // set arduino pin to output mode
-  digitalWrite(LIGHT_PIN, HIGH);      //defaults to lights OFF
+  digitalWrite(LIGHT_PIN, true);      //defaults to lights OFF
+  ledState = true;                    // the current state of LED low == NC switch == lights off
+  ledState2 = true;
+  ledState3 = true;
 }
 
 void loop() {
@@ -24,6 +27,8 @@ void loop() {
 
   // save the last state
   currentButtonState = digitalRead(BUTTON_PIN);  // read new state
+
+
   //check if ledState == ledState2 == ledState3, if so, do nothing, if not, set light_pin to the most common state
   if (!(ledState == ledState2 == ledState3)) {
     //set light_pin to the the ledstate with the most same values
@@ -37,16 +42,23 @@ void loop() {
   }
 
   //button state high = switch not pressed
-  if ((millis() - lasttime) > 5000) {
+  if ((millis() - lasttime) > 1000) {
 
     if (lastButtonState == HIGH && currentButtonState == LOW) {
       lasttime = millis();  // starts timer after switch toggled
 
       // toggle state of LED
       ledState = !ledState;
-      ledState2 = ledState;
+      ledState2 = !ledState2;
+      ledState3 = !ledState3;
       // control LED arccoding to the toggled state
-      digitalWrite(LIGHT_PIN, ledState);
+      if (ledState == ledState2) {
+        digitalWrite(LIGHT_PIN, ledState);
+      } else if (ledState == ledState3) {
+        digitalWrite(LIGHT_PIN, ledState);
+      } else if (ledState2 == ledState3) {
+        digitalWrite(LIGHT_PIN, ledState2);
+      }
     }
   }
 
